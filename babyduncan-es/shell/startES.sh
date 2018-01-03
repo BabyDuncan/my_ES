df037504ce64e107633ef463d7f0907cea8bae9b
guohaozhao <guohaozhao116008@sohu-inc.com>
2018-01-03 12:28:26 +0800

    fix

--- babyduncan-es/shell/startES.sh
+++ babyduncan-es/shell/startES.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-ES_PATH_CONF=../conf/node1 elasticsearch -d
-ES_PATH_CONF=../conf/node2 elasticsearch -d
+ES_PATH_CONF=../conf/node1 elasticsearch  -Enode.max_local_storage_nodes=2 -d
+ES_PATH_CONF=../conf/node2 elasticsearch  -Enode.max_local_storage_nodes=2 -d
